5d83341c4e40a6548038d71930fe1f6d2dd5642d
Denailly <danilocalheirosjr@gmail.com>
2022-06-15 01:00:17 -0300

    Criada estrutura react e abas

---
+++ src/pages/Entrada/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Entrada: React.FC = () => {
+    return(
+        <h1>Entrada</h1>
+    )
+}
+export default Entrada;
---
+++ src/pages/Saida/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Saida: React.FC = () => {
+    return(
+        <h1>Saida</h1>
+    )
+}
+export default Saida;
