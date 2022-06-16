47b761360c372b8b6e095cc18238aca04146749f
Denailly <danilocalheirosjr@gmail.com>
2022-06-16 04:54:18 -0300

    Criada lista de registro

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Dashboard from './pages/Dashboard';
+import ReactDOM from 'react-dom';
 
+import { ThemeProvider } from './hooks/theme';
+import { AuthProvider } from './hooks/auth';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>    
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
