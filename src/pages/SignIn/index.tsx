f3eee268ccc1d6083393f15532ee45b1ece1d3dc
Denailly <danilocalheirosjr@gmail.com>
2022-06-16 04:55:55 -0300

    adicionada tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, {useState} from 'react';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+    Container,
+    Logo,
+    Form,
+    FormTitle,
+} from './styles';
 
 const SignIn: React.FC = () => {
-    return(
-        <h1>SignIn</h1>
-    )
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');    
+
+    const { signIn } = useAuth();
+
+    return (
+        <Container>
+            <Logo>
+                <img src={logoImg} alt="Mr. Pig" />
+                <h2>Mr. Pig</h2>
+            </Logo>
+
+            <Form onSubmit={() => signIn(email, password)}>
+                <FormTitle>Entrar</FormTitle>
+
+                <Input 
+                    type="email"
+                    placeholder="e-mail"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input 
+                    type="password"
+                    placeholder="senha"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+               <Button type="submit">Acessar</Button>
+            </Form>
+        </Container>
+    );
 }
 
 export default SignIn;
