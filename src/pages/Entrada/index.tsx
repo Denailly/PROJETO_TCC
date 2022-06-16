36eee46abd24452ee97a01f19e96197483b348d0
Denailly <danilocalheirosjr@gmail.com>
2022-06-16 13:03:56 -0300

    adicionados assets styles e dashboard

--- src/pages/Entrada/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Entrada: React.FC = () => {
-    return(
-        <h1>Entrada</h1>
-    )
-}
-export default Entrada;
--- src/pages/Saida/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Saida: React.FC = () => {
-    return(
-        <h1>Saida</h1>
-    )
-}
-export default Saida;
